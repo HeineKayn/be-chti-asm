d5f133e63f2693a4a44f190aee8e08b21f7fd3db
HeineKayn <61800617+HeineKayn@users.noreply.github.com>
2021-04-07 13:26:29 +0200

    v1

--- Projet LASER, gestion du son/PjtKEIL_StepSon/Src/GestionSon.s
+++ Projet LASER, gestion du son/PjtKEIL_StepSon/Src/GestionSon.s
@@ -1,6 +1,8 @@
 	PRESERVE8
 	THUMB   
 		
+	EXPORT FlagCligno
+	EXPORT timer_callback
 
 ; ====================== zone de réservation de données,  ======================================
 ;Section RAM (read only) :
@@ -11,20 +13,36 @@
 	area    maram,data,readwrite
 		
 
-	
+FlagCligno dcw 0
 ; ===============================================================================================
-	
-
-
 		
 ;Section ROM code (read only) :		
 	area    moncode,code,readonly
 ; écrire le code ici		
 
-
-
-
-
+timer_callback proc
+	push {r4-r11,lr}
+	
+	ldr r1, =FlagCligno
+	ldr r0, [r1]
+	cmp r0, #0
+	bne diffzero
+	; égale à 0
+	mov r2, #1
+	mov r0,#1
+	bl GPIOB_Set
+	b fin
+	
+diffzero
+	mov r2, #0
+	mov r0,#1
+	bl GPIOB_Clear
+	
+fin
+	str r2, [r1]
+	pop {r4-r11,lr}
+	bx lr
+	endp	
 		
 		
 	END	
--- Projet LASER, gestion du son/PjtKEIL_StepSon/Src/principal.c
+++ Projet LASER, gestion du son/PjtKEIL_StepSon/Src/principal.c
@@ -13,7 +13,7 @@ int main(void)
 
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
-Timer_1234_Init_ff(TIM4, 3600000);
+Timer_1234_Init_ff(TIM4, 91*72);
 	
 // configuration du Timer 4 en débordement 100ms
 Active_IT_Debordement_Timer(TIM4, 2, timer_callback);	
