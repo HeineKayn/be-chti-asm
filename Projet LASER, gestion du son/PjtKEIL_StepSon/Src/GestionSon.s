7052dab626b91bede8849e2c524cf0777522c4ad
HeineKayn <61800617+HeineKayn@users.noreply.github.com>
2021-04-07 14:31:38 +0200

    Update GestionSon.s

--- Projet LASER, gestion du son/PjtKEIL_StepSon/Src/GestionSon.s
+++ Projet LASER, gestion du son/PjtKEIL_StepSon/Src/GestionSon.s
@@ -27,24 +27,27 @@ Compteur DCD 0
 timer_callback proc
 	push {r4-r11,lr}
 	
-	ldr r1, =Son
+	; Chargement des variables globales
+	ldr r1, =Son 		
 	ldr r3, =Compteur
 	ldr r2, [r3]
-	
 	ldr r7, =LongueurSon
 	ldr r7, [r7]
 	
+	; Si dépassement du tableau, on skip le code
 	cmp r2, r7
 	beq fin
 	
+	; La valeur était négative -> On la repasse en positive
 	ldrsh r0, [r1,r2, LSL#1]
 	add r0, #32768
+	; On la recentre sur [0;720]
 	mov r6, #91
 	sdiv r0, r0, r6
 	
+	; Actualisation des variables
 	ldr r5, =SortieSon
 	strh r0, [r5]
-	
 	add r2, #1  ;*Compteur + 1
 	str r2, [r3] ;Compteur++ 
 	
