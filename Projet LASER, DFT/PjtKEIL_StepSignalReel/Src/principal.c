70ec6349f85f349e567575dbe9d84965665e75dd
HeineKayn <61800617+HeineKayn@users.noreply.github.com>
2021-05-27 10:12:54 +0200

    Finition de step-dft + mise en place de dft-reel
    on doit tester avec les machines qui générent un signal

--- Projet LASER, DFT/PjtKEIL_StepSignalReel/Src/principal.c
+++ Projet LASER, DFT/PjtKEIL_StepSignalReel/Src/principal.c
@@ -3,9 +3,17 @@
 
 int DFT_ModuleAuCarre( short int * Signal64ech, char k);
 
-float Transformee[64];
+int Transformee[64];
 extern short int LeSignal[64];
 	
+void timer_callback(void){
+	Start_DMA1(64);
+	Wait_On_End_Of_DMA1();
+	Stop_DMA1;
+	for (int i=0; i<64; i++){
+		Transformee[i] = DFT_ModuleAuCarre(LeSignal, i);
+	}
+}
 
 int main(void)
 {
@@ -14,16 +22,20 @@ int main(void)
 // ============= INIT PERIPH (faites qu'une seule fois)  =====================
 // ===========================================================================
 
+Systick_Period_ff(5*72000);
+Systick_Prio_IT(10, timer_callback );
+SysTick_On;
+SysTick_Enable_IT;	
+	
+Init_TimingADC_ActiveADC_ff( ADC1, 72 );
+Single_Channel_ADC( ADC1, 2 );
+Init_Conversion_On_Trig_Timer_ff( ADC1, TIM2_CC2, 225 );
+short int dma_buf[64];
+Init_ADC1_DMA1(0, dma_buf );
+	
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
-for (int i=0; i<64; i++){
-	Transformee[i] = DFT_ModuleAuCarre(LeSignal, i);
-	//printf("%d -> %f \n",i,Transformee[i]);
-}
 
-
-	
-	
 
 //============================================================================	
 	
